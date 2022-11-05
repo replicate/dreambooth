b154b3cf09b0547a08743d38321c7663c0fb5d20
Zeke Sikelianos <zeke@sikelianos.com>
2022-11-04 21:37:00 -0700

    run latest version of model

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ MODEL_VERSION = "5c7d5dc6dd8bf75c1acaa8565735e7986bc5b66206b55cca93cb72c9bf15cca
 def train():
   print("Training model...")
   model = replicate.models.get(MODEL_NAME)
-  version = model.versions.get(MODEL_VERSION)
-  return version.predict(text="GitHub Actions")
+  # version = model.versions.get(MODEL_VERSION)
+  return model.predict(text="GitHub Actions")
 
 if __name__ == "__main__":
     output = train()
