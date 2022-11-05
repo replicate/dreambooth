6a9b5655aebea0576c30f70bc1b5f2218d7f4e00
Zeke Sikelianos <zeke@sikelianos.com>
2022-11-04 21:43:47 -0700

    typo

--- train.py
+++ train.py
@@ -2,16 +2,14 @@ import os
 import replicate
 import json
 
-MODEL_NAME = "replicate/hello-word"
+MODEL_NAME = "replicate/hello-world"
 MODEL_VERSION = "5c7d5dc6dd8bf75c1acaa8565735e7986bc5b66206b55cca93cb72c9bf15ccaa"
 
 def train():
-  # echo env var to make sure it's set
-  print("REPLICATE_API_TOKEN: ", os.environ["REPLICATE_API_TOKEN"])
   print("Training model...")
   model = replicate.models.get(MODEL_NAME)
-  # version = model.versions.get(MODEL_VERSION)
-  return model.predict(text="GitHub Actions")
+  version = model.versions.get(MODEL_VERSION)
+  return version.predict(text="GitHub Actions")
 
 if __name__ == "__main__":
     output = train()
