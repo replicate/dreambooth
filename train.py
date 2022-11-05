a92235b59f15eb2715bd494c9b660eae2e5437e1
Zeke Sikelianos <zeke@sikelianos.com>
2022-11-04 21:53:54 -0700

    log in before push

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
-import os
 import replicate
 import json
 
+# TODO: set this to the new DreamBooth training model
 MODEL_NAME = "replicate/hello-world"
 MODEL_VERSION = "5c7d5dc6dd8bf75c1acaa8565735e7986bc5b66206b55cca93cb72c9bf15ccaa"
 
@@ -9,9 +9,13 @@ def train():
   print("Training model...")
   model = replicate.models.get(MODEL_NAME)
   version = model.versions.get(MODEL_VERSION)
+
+  # TODO: collect image files from /data directory to use as input
   return version.predict(text="GitHub Actions")
 
 if __name__ == "__main__":
     output = train()
     print("Done training. Output:")
     print(json.dumps(output))
+
+    # TODO: download generated weights
