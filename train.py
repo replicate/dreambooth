b63ef8c0d3e20a93373fe43fa71b4ad5a17d3905
Zeke Sikelianos <zeke@sikelianos.com>
2022-11-04 21:41:35 -0700

    testing

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import replicate
 import json
 
@@ -5,6 +6,8 @@ MODEL_NAME = "replicate/hello-word"
 MODEL_VERSION = "5c7d5dc6dd8bf75c1acaa8565735e7986bc5b66206b55cca93cb72c9bf15ccaa"
 
 def train():
+  # echo env var to make sure it's set
+  print("REPLICATE_API_TOKEN: ", os.environ["REPLICATE_API_TOKEN"])
   print("Training model...")
   model = replicate.models.get(MODEL_NAME)
   # version = model.versions.get(MODEL_VERSION)
