80f8c61443e6c931dde250ac3a00eb64a69758d5
Zeke Sikelianos <zeke@sikelianos.com>
2022-11-15 15:47:23 -0800

    only extract images from instance data zip

--- predictor.py
+++ predictor.py
@@ -1,5 +1,6 @@
 import os
 import gc
+import mimetypes
 import shutil
 import tempfile
 from zipfile import ZipFile
@@ -206,8 +207,10 @@ class Predictor(BasePredictor):
             for zip_info in zip_ref.infolist():
                 if zip_info.filename[-1] == '/':
                     continue
-                zip_info.filename = os.path.basename(zip_info.filename)
-                zip_ref.extract(zip_info, cog_instance_data)
+                mt = mimetypes.guess_type(zip_info.filename)
+                if mt and mt[0] and mt[0].startswith('image/'):
+                    zip_info.filename = os.path.basename(zip_info.filename)
+                    zip_ref.extract(zip_info, cog_instance_data)
 
         if class_data is not None:
             with ZipFile(str(class_data), "r") as zip_ref:
